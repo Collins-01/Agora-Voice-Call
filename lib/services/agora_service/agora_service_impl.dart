2f704d3a46952d290d1993ccfd6cbaaa2d64ed6d
Collins <oriakhicolls01@gmail.com>
2024-03-04 01:31:19 +0100

    added http and socket initialization

--- lib/services/agora_service/agora_service_impl.dart
+++ lib/services/agora_service/agora_service_impl.dart
@@ -1,6 +1,35 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:agora_voice_call/services/agora_service/agora_service_interface.dart';
+import 'package:flutter/foundation.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+
+// ignore: constant_identifier_names
+const BASE_URL = 'https://voice-call-server.onrender.com';
+
+enum SocketConnectionStatus {
+  // ignore: constant_identifier_names
+  Connected,
+  // ignore: constant_identifier_names
+  Connecting,
+  // ignore: constant_identifier_names
+  Reconnecting,
+  // ignore: constant_identifier_names
+  Disconnected
+}
+
+extension SocketConnectionStatusExtension on SocketConnectionStatus {
+  bool get isConnecting => this == SocketConnectionStatus.Connecting;
+  bool get isDisconnected => this == SocketConnectionStatus.Disconnected;
+  bool get isConnected => this == SocketConnectionStatus.Connected;
+  bool get isReconnecting => this == SocketConnectionStatus.Reconnecting;
+}
 
 class AgoraServiceImpl extends IAgoraService {
+  late Socket _socket;
+  final ValueNotifier<SocketConnectionStatus> _connectionStatus =
+      ValueNotifier(SocketConnectionStatus.Disconnected);
   @override
   Future<void> emitIncomigCall(Map<String, dynamic> data) async {
     // TODO: implement emitIncomigCall
@@ -9,19 +38,98 @@ class AgoraServiceImpl extends IAgoraService {
 
   @override
   Future<void> init() async {
-    // TODO: implement init
-    throw UnimplementedError();
-  }
+    _socket = io(
+      BASE_URL,
+      OptionBuilder()
+          .setQuery({
+            //id of the current user
+            "user_id": '01',
+          })
+          .setTransports(['websocket']) // for Flutter or Dart VM
+          .disableAutoConnect() // disable auto-connection
+          .build(),
+    );
 
-  @override
-  Future<void> onIncomingCall() async {
-    // TODO: implement onIncomingCall
-    throw UnimplementedError();
+    _socket.onConnect((data) {
+      log("Socket connected...");
+      _connectionStatus.value = SocketConnectionStatus.Connected;
+    });
+    _socket.onReconnect(
+      (_) async {
+        log("Socket connection reconnecting");
+        _connectionStatus.value = SocketConnectionStatus.Reconnecting;
+      },
+    );
+    _socket.onDisconnect(
+      (data) {
+        log("Socket connection disconnected");
+        _connectionStatus.value = SocketConnectionStatus.Disconnected;
+      },
+    );
+
+    _socket.onConnectError(
+      (data) {
+        log("Socket connection error");
+      },
+    );
+
+    _socket.connect();
+    // ignore: unused_local_variable
+    Timer timer;
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (!_socket.connected) {
+        _socket.connect();
+      } else {
+        timer.cancel();
+      }
+    });
   }
 
   @override
   Future<String> getToken(String channel) {
     // TODO: implement getToken
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> emitCallMuteStatus(Map<String, dynamic> data) {
+    // TODO: implement emitCallMuteStatus
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> emitRejectCall(String userId) {
+    // TODO: implement emitRejectCall
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> emitUserOnAnotherCall(Map<String, dynamic> data) {
+    // TODO: implement emitUserOnAnotherCall
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> onRejectCall(data) {
+    // TODO: implement onRejectCall
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> onRemoteUserMuteCall(data) {
+    // TODO: implement onRemoteUserMuteCall
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> onUserOnAnotherCall(data) {
+    // TODO: implement onUserOnAnotherCall
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> onIncomingCall(Map<String, dynamic> data) {
+    // TODO: implement onIncomingCall
     throw UnimplementedError();
   }
 }
--- lib/services/agora_service/agora_service_interface.dart
+++ lib/services/agora_service/agora_service_interface.dart
@@ -1,10 +1,33 @@
 abstract class IAgoraService {
+  /// Initializes the socket and all other callbacks that needs to be initialized at the start of the project
   Future<void> init();
-  //***************************[  ]***************************** */
-  Future<void> onIncomingCall();
+  //***************************[ CALLBACKS ]***************************** */
+  /// A callback that is registered for `incoming calls`. It takes in a data which is returned from the socket data.
+  Future<void> onIncomingCall(Map<String, dynamic> data);
 
-  // **********************************s
+  /// A callback that is registered for when the receiver rejects a call.
+  Future<void> onRejectCall(dynamic data);
+
+  /// A callback that is registered for when the remote user mutes or unmutes tha call
+  Future<void> onRemoteUserMuteCall(dynamic data);
+
+  /// A callback that is registered for when the user is currently on another call.
+  Future<void> onUserOnAnotherCall(dynamic data);
+
+  // **********************************[ EVENTS ] *****************************************
+  /// emits and `incoming call` event to the socket server, passing the necessary information.
   Future<void> emitIncomigCall(Map<String, dynamic> data);
 
+  /// A method that allows the current user to reject a call.
+  Future<void> emitRejectCall(String userId);
+
+  /// A method that allows a user to send the `mute` status of the call.
+  Future<void> emitCallMuteStatus(Map<String, dynamic> data);
+
+  /// Emits an event to the caller to indicate that the receiver is currently on another call.
+  Future<void> emitUserOnAnotherCall(Map<String, dynamic> data);
+
+  //*************************************[METHODS]************************************************* */d for
+  /// Takes in the channel name as an argument, and returns a token from the server that will be used for the call. It can be null when no token is returned, or there is an error.
   Future<String> getToken(String channel);
 }
